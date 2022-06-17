6e6fba2d22e34ec2b65ad86c57a20a0c1e89dc47
Joshua Boyd <boyd.jc.github@gmail.com>
2022-06-16 22:18:44 -0400

    initial commit"

--- Assignment2b/ViewController.swift
+++ Assignment2b/ViewController.swift
@@ -13,6 +13,50 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    @IBOutlet weak var loanAmountOutlet: UITextField!
+        
+    @IBOutlet weak var numOfPmtsOutlet: UITextField!
+        
+    @IBOutlet weak var interestRateOutlet: UITextField!
+
+    @IBOutlet weak var pmtAmountOutlet: UILabel!
+        
+    @IBOutlet weak var pmtPeriodToggle: UISwitch!
+        
+    @IBAction func onTapGestureRecognized(_ sender: Any) {
+        loanAmountOutlet.resignFirstResponder()
+        numOfPmtsOutlet.resignFirstResponder()
+        interestRateOutlet.resignFirstResponder()
+    }
+        
+    @IBAction func onButtonPress(_ sender: UIButton) {
+            
+        let loanAmount = loanAmountOutlet.text! == "" ?  0.00 : Float(loanAmountOutlet.text!) ?? 0.00
+            
+        let numOfPmts = numOfPmtsOutlet.text! == "" ? 0 : Int(numOfPmtsOutlet.text!) ?? 0
+            
+        var interestRate = interestRateOutlet.text! == "" ? 0.00 : Float(interestRateOutlet.text!) ?? 0.00
+            
+        interestRate /= 100.0
+            
+        if(!pmtPeriodToggle.isOn) {
+            interestRate /= 12
+        }
+            
+        let fracPrePow = 1 + interestRate
+        let fracPow = pow(fracPrePow, Float(numOfPmts))
+
+        let formulaTop = interestRate * fracPow
+                
+        let formulaBottom = fracPow - 1
+                
+        let formulaFrac = formulaTop / formulaBottom
+            
+        let pmtAmount: Float = Float(loanAmount) * formulaFrac
+            
+        pmtAmountOutlet.text = "$" + String(round(pmtAmount * 100) / 100.0)
+    }
 
 
 }
